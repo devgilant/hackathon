2878eba831dd21d5e19adfe42f4d419be6ea87cf
devgilant <devgilant@gmail.com>
2016-02-21 17:44:30 +0200

    event with multiple soruces

--- models/Event.js
+++ models/Event.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var srcTypes = 'audio image video'.split(' ');
+var srcTypes = 'url audio image video'.split(' ');
 
 var eventSchema = new Schema({
   caption: String,
   description: String,
   startDate: Date,
   endDate: Date,
-  srcType: {type: String, enum: srcTypes},
-  srcURL: String
+  srcType: [{type: String, enum: srcTypes}],
+  srcURL: [String]
 });
 
 module.exports = mongoose.model('Event', eventSchema);
