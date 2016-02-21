ba34a0b204a54ca3e06b589037b11df292fce09b
devgilant <devgilant@gmail.com>
2016-02-21 09:07:26 +0200

    use vis.js timeline

--- public/js/controllers/timeline.js
+++ public/js/controllers/timeline.js
@@ -1,5 +1,13 @@
 timelineApp.controller('TimelineController', TimelineController);
 
+/*
+* Using vis.js, timelines have a data set of items, and optionally groups
+* for baselines.
+* Items are regular objects and can contain the properties 
+* start, end (optional), content, group (optional), className (optional), 
+* editable (optional), and style (optional)
+*/
+
 function TimelineController($scope, $http) {
     $scope.timelines = [];
     loadTimelines();
@@ -14,7 +22,27 @@ function TimelineController($scope, $http) {
         // call REST backend
         $http.get('/rest/timelines')
             .success(function(data) {
+                $.each(data, function(index, timeline){
+                    timeline.events = loadTimelineEvents(timeline.events);
+                });
                 $scope.timelines = data;
             });
     };
+
+    function loadTimelineEvents(events) {
+        // map to vis.js items
+        var items = events.map(function(timeline){
+         var item = {
+            start: timeline.startDate;
+            content: timeline.name;
+            description: timeline.description;
+            editable: false;
+         };
+         if (timeline.endDate)
+            item.end = timeline.endDate;
+         return item;
+        });
+        return items;
+    };
+
 }
